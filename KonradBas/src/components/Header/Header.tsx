e3ad5f5e2faeb7ecbeced26c6cee11a93916f199
Konradb211 <konradbas1234@gmail.com>
2026-04-25 16:11:35 +0200

    add scroll position hook and style navigation

--- KonradBas/src/components/Header/Header.tsx
+++ KonradBas/src/components/Header/Header.tsx
@@ -1,3 +1,5 @@
 export const Header = () => {
+  
+  
   return <header>Header</header>;
 };
--- KonradBas/src/components/NavigationBar/NavigationBar.tsx
+++ KonradBas/src/components/NavigationBar/NavigationBar.tsx
@@ -1,39 +1,45 @@
 import { Navigation } from "../../constants/Navigation";
+import { useScrollPosition } from "../../hooks/useScrollPosition";
 import styles from "./NavigationBar.module.scss";
 
 export const NavigationBar = () => {
+  const scrollPosition = useScrollPosition()
+
+
   return (
-    <nav className={styles.nav}>
-      <div className={`${styles.nav__container} container-wide`}>
-        <div>
-          <a href="/">Konrad Baś</a>
-        </div>
-        <div className={styles.nav__container__items}>
-          <ul className={styles.nav__container__items__container}>
-            {Navigation.map((item) => (
-              <li key={item.id}>
-                <a href={item.url} rel={item.rel} target={item.target}>
-                  {item.title}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className={styles.nav__container__icons}>
-          <a
-            href="https://github.com/Konradb211/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src="icons/github.svg" alt="Ikona githuba" />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/konrad-ba%C5%9B-34a223262/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src="icons/linkedin.svg" alt="Ikona linkedin" />
-          </a>
+    <nav className={`${styles.nav} ${scrollPosition > 50 ? styles.scrolled : ''}`}>
+      <div className={`${styles.nav__container} `}>
+        <div className={`${styles.nav__container__containerWide} container-wide`}>
+          <div>
+            <a href="/">Konrad Baś</a>
+          </div>
+          <div className={styles.nav__container__items}>
+            <ul className={styles.nav__container__items__container}>
+              {Navigation.map((item) => (
+                <li key={item.id}>
+                  <a href={item.url} rel={item.rel} target={item.target}>
+                    {item.title}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className={styles.nav__container__icons}>
+            <a
+              href="https://github.com/Konradb211/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src="icons/github.svg" alt="Ikona githuba" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/konrad-ba%C5%9B-34a223262/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src="icons/linkedin.svg" alt="Ikona linkedin" />
+            </a>
+          </div>
         </div>
       </div>
     </nav>
